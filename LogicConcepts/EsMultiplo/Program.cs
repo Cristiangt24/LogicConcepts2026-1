c7143fe79855a2fd854831b18119665cb104e6fa
Christiangt24 <cristiangutilds@hotmail.com>
2026-02-08 20:32:02 -0500

    complete exercice 04

---
+++ LogicConcepts/EsMultiplo/Program.cs
@@ -0,0 +1,27 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    Console.WriteLine("Hola veremos si dos numeros son multiplos o no");
+    var a = ConsoleExtension.GetInt("Ingrese primer número : ");
+    var b = ConsoleExtension.GetInt("Ingrese segundo número: ");
+    if (b % a == 0)
+    {
+        Console.WriteLine($"{b} es múltiplo de {a}");
+
+    }
+    else
+    {
+        Console.WriteLine($"{b} no es múltiplo de {a}");
+    }
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over.");
