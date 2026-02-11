4c7ff50e0a4a5c7473dbace035388286ea89ec73
Christiangt24 <cristiangutilds@hotmail.com>
2026-02-11 08:44:46 -0500

    complete exercices 06,07

---
+++ LogicConcepts/Descuentos/Program.cs
@@ -0,0 +1,36 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{ 
+    Console.WriteLine("Bienvenido al sistema de Descuentos");
+    int Despokt = ConsoleExtension.GetInt("Numero de escritorios a comprar: ");
+    var total = Despokt * 650000;
+
+    if (Despokt <= 5)
+    {
+        Console.WriteLine($"El numero de escritorios es: {Despokt}");
+        Console.WriteLine($"El descuento es del 10% y el total a pagar es: {Despokt * 650000 - (total* 0.90)}");
+    }
+        if (Despokt >= 5 && Despokt <=10)
+        {
+        Console.WriteLine($"El numero de escritorios es: {Despokt}");
+        Console.WriteLine($"El descuento es del 20% y el total a pagar es: {Despokt * 650000 - (total* 0.80)}");
+        }
+            if (Despokt >= 10)
+            {
+            Console.WriteLine($"El numero de escritorios es: {Despokt}");
+            Console.WriteLine($"El descuento es del 40% y el total a pagar es: {Despokt * 650000 - (total * 0.60)}");
+            }
+
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over.");
---
+++ LogicConcepts/NominaSencilla/Program.cs
@@ -0,0 +1,32 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{     
+    Console.WriteLine("Bienvenido al sistema de nómina sencilla");
+    var Usuario = ConsoleExtension.GetString("Ingrese el nombre del empleado: ");
+    var HorasTrabajadas = ConsoleExtension.GetDecimal("Ingrese el numero de horas trabajadas del empleado: ");
+    var ValorHora = ConsoleExtension.GetDecimal("Ingrese el valor por hora del empleado: ");
+    var Salario = ConsoleExtension.GetDecimal("Ingrese el salario minimo del mes ");
+    var SalarioTotal = HorasTrabajadas * ValorHora;
+
+    if (SalarioTotal > Salario)
+    {
+        Console.WriteLine($"Empleado:  {Usuario}");
+        Console.WriteLine($"Salario: {SalarioTotal}");
+        Console.WriteLine("El empleado Gana mas del salario Minimio");
+    }
+    else
+    {
+        Console.WriteLine($"El Nombre del empleado es: {Usuario}");
+    }
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over.");
