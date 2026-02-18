0e1003bdffefabb3ec8c98f8bdaa2051750747c7
Christiangt24 <cristiangutilds@hotmail.com>
2026-02-18 12:09:32 -0500

    complete exercise 13

---
+++ LogicConcepts/FactorialNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var number = ConsoleExtension.GetInt("Ingrese número: ");
+    var factorial = MyMath.Factorial(number);
+    Console.WriteLine($"El Factorial Es: {factorial:N0}");
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over.");
